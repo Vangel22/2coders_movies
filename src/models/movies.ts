3f1f1175e40715d71dc1f5cba2e5c4d379d01c65
Vangel22 <h.vangel22@gmail.com>
2022-10-03 21:10:01 +0200

    Movies model

---
+++ src/models/movies.ts
@@ -0,0 +1,16 @@
+export type Movies = {
+    adult: string;
+    backdrop_path: string;
+    genre_ids: Array<number>;
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: string;
+    poster_path: string;
+    release_date: Date;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
